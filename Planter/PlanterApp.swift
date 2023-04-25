f4814b24bfca1fa7d3271bb63c9355a572cb70af
Juan Guerrero <broncos10499@yahoo.com>
2023-04-25 15:11:35 -0700

    Initial Commit

---
+++ Planter/PlanterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanterApp.swift
+//  Planter
+//
+//  Created by Juan Guerrero on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
